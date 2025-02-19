aab08d215c9a6dec0b1b575f9ab2207f186e2f14
SHYY <pinting63@gmail.com>
2025-02-19 11:55:00 +0800

    week01

---
+++ week01-1.cpp
@@ -0,0 +1,15 @@
+/// week01-1.cpp瘋狂程設 兩數相加
+/// File-New empty 開新的檔案
+/// Ctrl-s 存檔程week01-1.cpp 要記得加 .cpp
+/// 下周考試考這題, 不用寫藍色的「中文註解」
+#include <stdio.h> ///檔名不能寫錯
+/// stdio 的意思是 標準的standard input/output
+/// 有了 stdio.h 的外掛, 才能用 scanf() printf()
+int main()
+{
+    int a, b; /// 整數 a,b
+    scanf( "%d" , &a);///正課剛剛教,掃描讀東西
+    scanf( "%d" , &b);///可以用複製的,再改b
+
+    printf("%d", a+b );///實習課有教,印東西
+}
